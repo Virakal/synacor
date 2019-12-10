17e8344b1dc7a308dc090397ac8107902f28a207
Jon Goodger <jonno.is@gmail.com>
2019-12-10 05:23:35 +0000

    Tidy up memory code

--- synacor/memory.py
+++ synacor/memory.py
@@ -1,5 +1,5 @@
 class Memory(object):
-    _memory = {}
+    memory = {}
 
     MAX_MEMORY_OFFSET = 32775
 
@@ -7,20 +7,21 @@ class Memory(object):
         self.validate_key(key)
 
         try:
-            return self._memory[key]
+            return self.memory[key]
         except:
+            # Empty memory segments are just nulled out
             return 0
 
     def __setitem__(self, key: int, value: int):
         self.validate_key(key)
         self.validate_value(value)
-        self._memory.__setitem__(key, value)
+        self.memory.__setitem__(key, value)
 
     def __len__(self):
-        return len(self._memory)
+        return len(self.memory)
 
     def __repr__(self, *args):
-        return self._memory.__repr__(*args)
+        return self.memory.__repr__(*args)
 
     def validate_key(self, key):
         if not type(key) == int:
@@ -30,7 +31,9 @@ class Memory(object):
             raise KeyError("Memory index must not be negative")
 
         if key > self.MAX_MEMORY_OFFSET:
-            raise KeyError(f"Memory index must be between 0 and {self.MAX_MEMORY_OFFSET}")
+            raise KeyError(
+                f"Memory index must be between 0 and {self.MAX_MEMORY_OFFSET}"
+            )
 
     def validate_value(self, value):
         if not type(value) == int:
