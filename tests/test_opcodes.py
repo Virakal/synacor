6e8eea80979e9f119928ddc13b45bd7cb196ce66
Jon Goodger <jonno.is@gmail.com>
2019-12-11 20:52:59 +0000

    Test Mult opcode

--- tests/test_opcodes.py
+++ tests/test_opcodes.py
@@ -17,3 +17,10 @@ class MemoryTests(unittest.TestCase):
         self.op.op_add(32769, 32758, 15)
         self.assertEquals(self.memory[32769], 5)
 
+    def test_mod(self):
+        self.op.op_mult(32768, 40, 99)
+        self.assertEquals(self.memory[32768], 40 * 99)
+
+        # Modulo Test
+        self.op.op_mult(32769, 32758, 10)
+        self.assertEquals(self.memory[32769], 32668)
